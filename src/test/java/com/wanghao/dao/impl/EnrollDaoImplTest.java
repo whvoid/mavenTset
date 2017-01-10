d0452db9f34bb622e82c306c1472e92649af3507
wanghao <87025569@qq.com>
2017-01-10 18:11:33 +0800

    log4j
    加入了log4j

--- src/test/java/com/wanghao/dao/impl/EnrollDaoImplTest.java
+++ src/test/java/com/wanghao/dao/impl/EnrollDaoImplTest.java
@@ -2,14 +2,19 @@ package com.wanghao.dao.impl;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import com.wanghao.domain.Enroll;
 public class EnrollDaoImplTest {
     EnrollDaoImpl EnrollDaoImpl=new EnrollDaoImpl();
+    Logger logger = Logger.getLogger(this.getClass());  
+    
     @Test
     public void testAdd() throws SQLException {
 	Enroll enroll=new Enroll(1, new Date().getTime(), new Date().getTime(), "王昊", "5555", "2017年1月9日", "java46班","www.baidu.com", "我最帅", "刘德华");
 	EnrollDaoImpl.add(enroll);
+	logger.info("添加测试完成");
     }
 
     @Test
